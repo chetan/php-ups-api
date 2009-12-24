4e4da06e6070afe4f9d09ed7d1137c36c3877527
Chetan Sarva <chetan@pixelcop.net>
2009-12-23 23:45:54 -0500

    removed dependency on config

--- scripts/phpdoc.php
+++ scripts/phpdoc.php
@@ -1,16 +1,14 @@
 <?php
-/**
- * Include the configuration file
- */
-require_once dirname(__FILE__).'/../inc/config.php';
+
+$base = dirname(dirname(__FILE__));
 
 $files = array(
-	BASE_PATH.'/UpsAPI.php',
+    "$base/UpsAPI.php",
 ); // end $files
 
 $directories = array(
-	BASE_PATH.'/UpsAPI'
+	"$base/UpsAPI"
 );
 
-exec('php '.BASE_PATH.'/inc/pear/PhpDocumentor/phpdoc -f '.implode(',', $files).' -d '.implode(',', $directories).' -o HTML:frames:earthli -t '.BASE_PATH.'/doc/phpdoc', $result);
+exec('phpdoc -f '.implode(',', $files).' -d '.implode(',', $directories).' -o HTML:frames:earthli -t  '.$base.'/doc/phpdoc', $result);
 print_r($result);
